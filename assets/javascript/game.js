9bdb2c6650883556a8686459470479886716582f
Phil Mauracher <gradientus@gmail.com>
2019-07-12 15:57:37 -0600

    I think I'm pretty close on this one.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
-  var randomScore = Math.floor(Math.random() * 120) + 10; //this will generate the a random goal score from 20 to 100
-  var randomGemValue1 = Math.floor(Math.random() * 1) + 1; //this will generate a value for one of the gems
-  var randomGemValue2 = Math.floor(Math.random() * 10) + 2; //this will generate a value for another of the gems
-  var randomGemValue3 = Math.floor(Math.random() * 15) + 2; //this will generate a value for another of the gems
-  var randomGemValue4 = Math.floor(Math.random() * 35) + 2; //this will generate a value for another of the gems
+  //these will establish the goal and the random values attached to each crystal
+  var randomScore = Math.floor(Math.random() * 120) + 10;
+  var randomGemValue1 = Math.floor(Math.random() * 4) + 1;
+  var randomGemValue2 = Math.floor(Math.random() * 8) + 2;
+  var randomGemValue3 = Math.floor(Math.random() * 16) + 3;
+  var randomGemValue4 = Math.floor(Math.random() * 32) + 4;
+
   var gemValues = [
     randomGemValue1,
     randomGemValue2,
@@ -11,12 +13,11 @@ $(document).ready(function() {
     randomGemValue4
   ]; //this array contains the possible values
   var gemPath = [
-    "../images/jade.png",
-    "../images/ruby.png",
-    "../images/sapphire.png",
-    "../images/topaz.png"
+    "assets/images/jade.png",
+    "assets/images/ruby.png",
+    "assets/images/sapphire.png",
+    "assets/images/topaz.png"
   ];
-
   var currentScore = 0;
   var wins = 0;
   var losses = 0;
@@ -25,4 +26,21 @@ $(document).ready(function() {
   $(".currentScore").text("Current Score: " + currentScore);
   $(".wins").text("Wins: " + wins);
   $(".losses").text("Losses: " + losses);
+
+  for (var i = 0; i < gemPath.length; i++) {
+    var gemPic = $("<img>");
+    gemPic.addClass("gems");
+    gemPic.attr("data-value", gemValues[i]);
+    gemPic.attr("src", gemPath[i]);
+    $("#gemsBox").append(gemPic);
+  }
+
+  $(".gems").on("click", function() {
+    //why does $(___) have to be the class instead of the image element?
+    console.log($(".gems")); //a little stuck here.  GAAAHHHHH!
+    //get the value from the data value using attr (?), not fully sure how to do that, look at past exercises
+    //add the value pulled from the gems data value to the currentScore
+    //if the current score > randomScore then you lose, losses++ (maybe indicate that somehow)
+    //if the current scor === randomScore then you win, win++ (maybe indicate that too)
+  });
 });
